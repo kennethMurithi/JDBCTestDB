3a4599132d2263967bd13e13e2fd4a9f41ce5891
[Kenneth Murithi] <[Kennethmmuriithi@gmail.com]>
2022-04-15 09:51:17 +0300

    Inserting, update and delete data from contacts table and retrieving to output terminal. Closes #2

--- src/com/hiken/Main.java
+++ src/com/hiken/Main.java
@@ -1,9 +1,6 @@
 package com.hiken;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -12,14 +9,26 @@ public class Main {
 
             Connection conn = DriverManager.getConnection("Jdbc:sqlite:E:\\Tutorial Materials\\SpringBoot\\TestDB\\testjava.db");
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
-                            "(name TEXT, phone INTEGER, email TEXT)");
-            statement.execute("INSERT INTO contacts(name, phone, email)" +
-                            "VALUES('Ken', 07232222,'ken@email.com')");
-            statement.execute("INSERT INTO contacts(name, phone, email)" +
-                    "VALUES('Eva', 0987222,'eva@pmain.com')");
-            statement.execute("INSERT INTO contacts(name, phone, email)" +
-                    "VALUES('Joel', 0987662,'joel@fcome.com')");
+//            statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
+//                            "(name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts(name, phone, email)" +
+//                            "VALUES('Ken', 07232222,'ken@email.com')");
+//            statement.execute("INSERT INTO contacts(name, phone, email)" +
+//                            "VALUES('Eva', 0987222,'eva@pmain.com')");
+//            statement.execute("INSERT INTO contacts(name, phone, email)" +
+//                            "VALUES('Joel', 0987662,'joel@fcome.com')");
+
+//            statement.execute("UPDATE contacts SET phone='0982222' where name='Ken' ");
+//            statement.execute("DELETE FROM contacts WHERE name='Joel'");
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()){
+                System.out.println(results.getString("name") + " "
+                                    + results.getInt("phone") + " "
+                                    + results.getString("email"));
+            }
+            results.close();
 
             statement.close();
             conn.close();
